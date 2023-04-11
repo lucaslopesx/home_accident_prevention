a1198c5397331a5ca40ef77e751460236ca98443
Lucas Lopes <lucas.lopesx1@gmail.com>
2023-04-10 21:34:02 -0300

    fix: identation

--- lib/main.dart
+++ lib/main.dart
@@ -26,70 +26,74 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xFF0C2E8B),
-        body: Column(
-          children: [
-            SafeArea(
-              child: Container(
-                margin: const EdgeInsets.only(top: 20),
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    'Olá amiguinho (a)!',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.fredoka(
-                      fontSize: 40,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
+      backgroundColor: const Color(0xFF0C2E8B),
+      body: Column(
+        children: [
+          SafeArea(
+            child: Container(
+              margin: const EdgeInsets.only(top: 20),
               child: Align(
-                alignment: Alignment.center,
-                child: Text("Prevenção \nde acidentes domésticos",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.fredoka(
-                      fontSize: 45,
-                      color: Colors.white,
-                    )),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-                color: const Color(0xFFF4BD18),
-                border: Border.all(
-                  width: 2,
-                  color: const Color(0xFFF7CF5A),
-                ),
-              ),
-              child: TextButton(
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                    const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 16.0),
+                alignment: Alignment.topCenter,
+                child: Text(
+                  'Olá amiguinho (a)!',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.fredoka(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-                onPressed: () => {},
-                child: Text("VAMOS NESSA!",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.fredoka(
-                        fontSize: 30,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500)),
               ),
             ),
-            FractionallySizedBox(
-              widthFactor: 0.9,
-              child: Image.asset(
-                'images/home.png',
-                fit: BoxFit.fitWidth,
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                "Prevenção \nde acidentes domésticos",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.fredoka(
+                  fontSize: 45,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ],
-        ));
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              color: const Color(0xFFF4BD18),
+              border: Border.all(
+                width: 2,
+                color: const Color(0xFFF7CF5A),
+              ),
+            ),
+            child: TextButton(
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                ),
+              ),
+              onPressed: () => {},
+              child: Text(
+                "VAMOS NESSA!",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.fredoka(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+          ),
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: Image.asset(
+              'images/home.png',
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
